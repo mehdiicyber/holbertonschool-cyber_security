437b36473b5096c0a5679c5c4b3d0aba22a5646d
mehdiicyber <13735@holbertonstudents.com>
2026-05-09 22:49:51 +0400

    Update 6-smtp.sh

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-TLS_LEVEL=$(grep "^smtpd_tls_security_level =" /etc/postfix/main.cf | cut -d' ' -f3)
-if [ -z "$TLS_LEVEL" ]; then
-    echo "STARTTLS not configured"
-else
-    echo "smtpd_tls_security_level = $TLS_LEVEL"
-fi
+grep "^smtpd_tls_security_level =" /etc/postfix/main.cf || echo "STARTTLS not configured"
