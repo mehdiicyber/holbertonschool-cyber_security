b0692ae4f2f9dede625a29d9846418c8042c39cb
mehdiicyber <13735@holbertonstudents.com>
2026-05-26 13:51:51 +0400

    Update 1-add_group.sh

--- linux_security/0x01_permissions_sguid_sgid/1-add_group.sh
+++ linux_security/0x01_permissions_sguid_sgid/1-add_group.sh
@@ -1,4 +1,6 @@
+cat << 'EOF' > 1-add_group.sh
 #!/bin/bash
-[ -n "$1" ] && [ -f "$2" ] || exit 1
-groupadd -f "$1"
-chown :"$1" "$2" && chmod g+rx "$2"
+groupadd "$1"
+chown :"$1" "$2"
+chmod g+rx "$2"
+EOF
