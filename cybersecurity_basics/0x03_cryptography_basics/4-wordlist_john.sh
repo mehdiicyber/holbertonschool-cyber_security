963a7ab43ae409650ac731adb180475fc4be49c3
mehdiicyber <13735@holbertonstudents.com>
2026-05-28 21:09:20 +0400

    Update 4-wordlist_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --show "$1" | awk -F: '/^[a-zA-Z0-9]/ {print $2}' | sed '/^$/d' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --show "$1" | awk -F: '/^\?/ {print $2}' > 4-password.txt
