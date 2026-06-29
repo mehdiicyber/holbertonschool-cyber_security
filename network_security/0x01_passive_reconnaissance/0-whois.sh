ed6158f504b9f8a369e4276d0bbae653ec5e499f
mehdiicyber <13735@holbertonstudents.com>
2026-06-29 19:29:30 +0400

    Update 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '
-BEGIN {
-    split("Registrant Admin Tech", g);
-    split("Name Organization Street City State/Province Postal Code Country Phone Phone Ext: Fax Fax Ext: Email", f);
-}
-{
-    gsub(/^[ \t]+|[ \t]+$/, "", $1);
-    gsub(/^[ \t]+|[ \t]+$/, "", $2);
-    d[$1] = $2;
-}
-END {
-    for (i=1; i<=3; i++) {
-        for (j=1; j<=12; j++) {
-            k = g[i] " " f[j]; sub(/ Ext:/, "", k);
-            v = d[k];
-            if (f[j] == "Street" && v) v = v " ";
-            printf "%s%s,%s", (m++ ? "\n" : ""), g[i] " " f[j], v;
-        }
-    }
-}' > "$1.csv"
+whois "$1" | awk -F': ' 'BEGIN{split("Registrant Admin Tech",g);split("Name Organization Street City State/Province Postal Code Country Phone Phone Ext: Fax Fax Ext: Email",f)}{gsub(/^[ \t]+|[ \t]+$/,"",$1);gsub(/^[ \t]+|[ \t]+$/,"",$2);d[$1]=$2}END{for(i=1;i<=3;i++){for(j=1;j<=12;j++){k=g[i]" "f[j];sub(/ Ext:/,"",k);v=d[k];if(f[j]=="Street"&&v)v=v" ";printf "%s%s,%s",(m++?"\n":""),g[i]" "f[j],v}}}' > "$1.csv"
