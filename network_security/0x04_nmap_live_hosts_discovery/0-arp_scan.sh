9460c5bf58ffcf32c5881a23b57c353ad6c8c7d7
mehdiicyber <13735@holbertonstudents.com>
2026-06-29 18:18:39 +0400

    Update 0-arp_scan.sh

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Usage: $0 <subnetwork>"
-    echo "Example: $0 192.168.65.0/24"
-    exit 1
-fi
-if [ "$EUID" -ne 0 ]; then
-    echo "Error: This script must be run as root or with sudo."
-    exit 1
-fi
-SUBNET="$1"
-nmap -PR -sn "$SUBNET"
+[ "$EUID" -ne 0 ] && echo "Root yetkisi lazımdır." && exit 1 || nmap -PR -sn "$1"
