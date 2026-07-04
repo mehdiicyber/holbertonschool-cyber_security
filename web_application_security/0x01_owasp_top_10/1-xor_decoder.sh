94eece42f7ff23a644650463a712388e5210b69f
mehdiicyber <13735@holbertonstudents.com>
2026-07-04 09:36:04 +0400

    Create 1-xor_decoder.sh

---
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "İstifadə qaydası: $0 <hash>"
+    exit 1
+fi
+
+encoded_str="${1#\{xor\}}"
+
+hex_bytes=$(echo -n "$encoded_str" | base64 -d 2>/dev/null | xxd -p | tr -d '\n')
+
+if [ -z "$hex_bytes" ]; then
+    echo "Xəta: Doğru Base64 formatı deyil."
+    exit 1
+fi
+
+decoded_output=""
+for (( i=0; i<${#hex_bytes}; i+=2 )); do
+    hex_byte="${hex_bytes:i:2}"
+    
+    dec_byte=$((16#$hex_byte))
+    
+    xor_byte=$((dec_byte ^ 95))
+    
+    decoded_output+="$(printf "\\$(printf '%03o' "$xor_byte")")"
+done
+
+echo "$decoded_output"
