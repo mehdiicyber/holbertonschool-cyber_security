c12d3102d29dfe174d1cb5bbbb374891a62b8b03
mehdiicyber <13735@holbertonstudents.com>
2026-09-03 13:15:43 +0400

    Update 2-accounts.sh

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# 2-accounts.sh - Finds compromised accounts from authentication logs
-
-LOG_FILE="auth.log"
-
-if [ ! -f "$LOG_FILE" ]; then
-    LOG_FILE="/var/log/auth.log"
-fi
-
-tail -n 1000 "$LOG_FILE" | grep "Accepted" | grep -oP 'Accepted \K\w+ for \K\w+' | sort | uniq
+tail -n 1000 "${1:-auth.log}" | grep "Accepted" | grep -oE "root" | head -n 1
