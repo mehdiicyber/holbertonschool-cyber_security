398b7c1f0b6c0228acd28c61e5e1cfd8368d8e00
mehdiicyber <13735@holbertonstudents.com>
2026-09-03 13:19:17 +0400

    Update 3-ips.sh

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# 3-ips.sh - Counts distinct IP addresses that successfully logged in
-
-LOG_FILE="${1:-auth.log}"
-
-grep "Accepted" "$LOG_FILE" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}" | sort -u | wc -l
+grep "Accepted" "${1:-auth.log}" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}" | sort -u | wc -l
