8134b8c45b8a5e2ca83f2f30e6b846d9c4aefc3a
mehdiicyber <13735@holbertonstudents.com>
2026-09-05 15:11:26 +0400

    Update 4-firewall.sh

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-grep -iE "iptables|ufw" "${1:-auth.log}" | grep -iE "\-A|\-I|add|allow" | wc -l | xargs
+#
+# 4-firewall.sh
+# Counts how many firewall rules have been added,
+# based on entries in auth.log.
+
+LOGFILE="/var/log/auth.log"
+
+# Look for sudo/root commands that used iptables or ufw to ADD a rule.
+# -A (append) / -I (insert) for iptables, and "ufw allow"/"ufw deny"/"ufw insert" for ufw.
+grep -iE "COMMAND=.*(iptables.*(-A|-I|--append|--insert)|ufw (allow|deny|insert|reject))" "$LOGFILE" | wc -l
