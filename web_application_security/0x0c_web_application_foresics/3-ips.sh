6709cd3e2c95f106b0f330fe468fd38959e3b2ba
mehdiicyber <13735@holbertonstudents.com>
2026-09-08 13:32:07 +0400

    Update 3-ips.sh

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "Accepted" auth.log | awk '{for(i=1;i<=NF;i++) if ($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $i}' | sort -u | wc -l
+grep "POST /login" "$LOGFILE" | grep " 200 " | awk '{print $1}' | sort -u | wc -l
